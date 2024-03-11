c64651eb7cff77e2de658c433715b1c54d44731e
Gasatrya <gasatrya.dev@gmail.com>
2024-03-11 20:45:54 +0700

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '921cb1551fbc7c64d820');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e2b9fec931500256af7c');
